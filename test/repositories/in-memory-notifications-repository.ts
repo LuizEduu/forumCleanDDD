62864b1856ff9dc1115e5664f637083f79398968
Luiz <l.eduardo8316@gmail.com>
2024-03-25 21:31:03 -0300

    feat: mark notification to read use case

--- test/repositories/in-memory-notifications-repository.ts
+++ test/repositories/in-memory-notifications-repository.ts
@@ -4,9 +4,23 @@ import { Notification } from '@/domain/notification/enterprise/entities/notifica
 export class InMemoryNotificationsRepository
   implements NotificationsRepository
 {
-  public items: Notification[] = []
+  private notifications: Notification[] = []
 
   async create(notification: Notification) {
-    this.items.push(notification)
+    this.notifications.push(notification)
+  }
+
+  async save(notification: Notification): Promise<void> {
+    const index = this.notifications.findIndex(
+      (n) => n.id.toString() === notification.id.toString(),
+    )
+
+    this.notifications[index] = notification
+  }
+
+  async findById(id: string): Promise<Notification | null> {
+    const notification = this.notifications.find((n) => n.id.toString() === id)
+
+    return notification ?? null
   }
 }
